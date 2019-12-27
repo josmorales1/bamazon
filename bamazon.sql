326622facb0cd37b47e48c448fbfd4ab9cf0f12d
josmorales1 <josgerardomorales1@aol.com>
2019-12-26 19:33:21 -0600

    finished with the database and the customer.js

---
+++ bamazon.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE database bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products
+(
+    item_id INT(11) NOT NULL,
+    product_name VARCHAR(30) NOT NULL,
+    department_name VARCHAR(30) NOT NULL,
+    price DECIMAL(20,2) NOT NULL,
+    stock_quantity INT(11) NOT NULL,
+    PRIMARY KEY (item_id)
+);
+
+
+SELECT *
+FROM products;
+
+INSERT INTO products
+    (item_id, product_name, department_name, price, stock_quantity)
+VALUES
+    (1, "Flash Drives", "Electronics", 4.99, 20),
+    (2, "Controller", "Electronics", 15.99, 10),
+    (3, "Video Game", "Electronics", 59.99, 5),
+    (4, "Iphone X", "Electronics", 1029.99, 14),
+    (5, "PS4", "Electronics", 239.99, 5),
+    (6, "keyboards", "Electronics", 19.99, 30),
+    (7, "XBOXONE", "Electronics", 149.99, 14),
+    (8, "mouse", "Electronics", 9.99, 15),
+    (9, "Samsung Tv", "Electronics", 199.99, 20),
+    (10, "MacBook Air", "Electronics", 699.99, 10)
---
+++ bamazonCustomer.js
@@ -0,0 +1,74 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazon_db"
+});
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id" + connection.threadId);
+});
+
+var displayProducts = function () {
+    var query = "Select * FROM products";
+    connection.query(query, function (err, res) {
+        if (err) throw err;
+        var displayTable = new Table({
+            head: ["item_id", "product_name", "department_name", "price", "stock_quantity"],
+            colWidths: [10, 25, 25, 10, 14]
+        });
+        for (var i = 0; i < res.length; i++) {
+            displayTable.push(
+                [res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            );
+        }
+        console.log(displayTable.toString());
+        purchasePrompt();
+    });
+}
+
+function purchasePrompt() {
+    inquirer.prompt([
+        {
+            name: "id",
+            type: "input",
+            message: "enter item id you would like to purhcase.",
+            filter: mumber
+        },
+        {
+            name: "quantity",
+            type: "input",
+            message: "How many items do you wish to purchase?",
+            filter: Number
+        },
+
+    ]).then(function (answers) {
+        var IDrequested = answers.ID;
+        var quantityNeeded = answers.Quantity;
+        purchaseOrder(IDrequested, quantityNeeded);
+    });
+};
+
+function purchaseOrder(ID, totalNeeded) {
+    connection.query('Select * FROM products WHERE item_id =' + ID, function (err, res) {
+        if (err) { console.log(err) };
+        if (totalNeeded <= res[0].stock_quantity) {
+            var totalPrice = res[0].price * totalNeeded;
+            console.log("Good news your order is in stock!");
+            console.log("Your total cost for " + totalNeeded + " " + res[0].product_name + " is " + totalPrice + " Thank you!");
+
+            connection.query("UPDATE products SET stock_quantity = stock_quantity - " + totalNeeded + "WHERE item_id = " + ID);
+        } else {
+            console.log("Insufficient quantity, sorry we do not have enough " + res[0].product_name + "to complete your order.");
+        };
+        displayProducts();
+    });
+};
+displayProducts();
+
